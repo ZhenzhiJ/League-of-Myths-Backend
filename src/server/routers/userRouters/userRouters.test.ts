a39502cb1a5a68893532ec83d45051230a9617da
Zhenzhi Jin <zhenzhi97@gmail.com>
2022-11-23 12:02:09 +0100

    Test register endpoint with supertest

---
+++ src/server/routers/userRouters/userRouters.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import connectDatabase from "../../../database/connectDatabase";
+import app from "../../app";
+
+let server: MongoMemoryServer;
+
+beforeAll(async () => {
+  server = await MongoMemoryServer.create();
+  await connectDatabase(server.getUri());
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await server.stop();
+});
+
+describe("Given the method POST and the endpoint '/users/register'", () => {
+  const newUser = {
+    username: "pikachu",
+    password: "pikachu",
+    email: "pokachu@chu.com",
+  };
+
+  describe("When it receives a request with username 'pikachu', password 'pikachu' and email 'pokachu@chu.com'", () => {
+    test("Then it should respond with a 201 and a property user", async () => {
+      const response = await request(app)
+        .post("/users/register")
+        .send(newUser)
+        .expect(201);
+
+      expect(response.body).toHaveProperty("user");
+    });
+  });
+});
