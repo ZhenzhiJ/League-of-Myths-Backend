4846876ff9546b09cfa032d59825f302b88abb7f
Zhenzhi Jin <zhenzhi97@gmail.com>
2022-12-04 22:14:15 +0100

    Add delete route

--- src/server/routers/championRouters/championRouters.ts
+++ src/server/routers/championRouters/championRouters.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { loadChampions } from "../../controllers/championControllers/championControllers.js";
+import {
+  deleteChampion,
+  loadChampions,
+} from "../../controllers/championControllers/championControllers.js";
+import routes from "../routes.js";
 
 // eslint-disable-next-line new-cap
 const championRouter = express.Router();
 
+const { deleteRoute } = routes;
+
 championRouter.get("", loadChampions);
+championRouter.delete(deleteRoute, deleteChampion);
 
 export default championRouter;
